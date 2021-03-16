bdd43eb3a0ead1d5b8f468276eb5e36d7b71c67d
mlblogss <80729769+mlblogss@users.noreply.github.com>
2021-03-16 10:55:44 +0800

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -38,7 +38,7 @@ O),s=s.replace(Q,""),g.jsExtRegExp.test(s)&&(s=I),q.deps=q.deps?q.deps.concat(s)
 
 tbquire.config({
 	baseUrl: '//pic.mlcdn.chineseoh.com/themes/dux/js',
-	urlArgs: ,
+	urlArgs: '',
 	paths: {
 		'swiper'        : 'libs/swiper.min',
 		'jquery.cookie' : 'libs/jquery.cookie.min',
